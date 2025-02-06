f2527753d2ee91a163e96a67f94a8c3942b4f4a4
yavuztokus <yavuztokus2@gmail.com>
2025-02-06 14:05:12 +0000

    first iteration

--- src/pages/ResultVisualization/ResultsVisualization.tsx
+++ src/pages/ResultVisualization/ResultsVisualization.tsx
@@ -1,7 +1,10 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+
 import "./ResultsVisualization.css";
 
 const ResultsVisualization: React.FC = () => {
+  const [gridData, setGridData] = useState([]);
+
   // Fetch initial grid data from API
   useEffect(() => {
     const fetchGridData = async () => {
@@ -12,11 +15,13 @@ const ResultsVisualization: React.FC = () => {
       // Wait for the response data to be parsed as JSON
       const jsonData = await allDataResponse.json();
 
-      console.log("Grid data:", jsonData);
+      setGridData(jsonData.iterations);
     };
 
     fetchGridData();
   }, []);
+
+  console.log("Grid data:", gridData);
 
   return (
     <div className="container">
@@ -25,7 +30,18 @@ const ResultsVisualization: React.FC = () => {
         <button className="button">pressure</button>
         <button className="button">temperature 2</button>
       </div>
-      <div className="grid">boxes</div>
+      <div className="grid">
+        {gridData.length > 0 &&
+          gridData.map((data, index) => (
+            <div key={index}>
+              {data.values.map((value, valueIndex) => (
+                <div key={valueIndex} className="box">
+                  <div className="kelvin">{value[2].kelvin}</div>
+                </div>
+              ))}
+            </div>
+          ))}
+      </div>
       <div className="controls">
         <div>
           <button className="button">Play</button>
