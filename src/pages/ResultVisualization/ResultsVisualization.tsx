6d48904e59416aac199760a1b3b10ef45b1c0b10
yavuztokus <yavuztokus2@gmail.com>
2025-02-06 12:58:46 +0000

    creating the frame

--- src/pages/ResultVisualization/ResultsVisualization.tsx
+++ src/pages/ResultVisualization/ResultsVisualization.tsx
@@ -3,17 +3,6 @@ import "./ResultsVisualization.css";
 
 const ResultsVisualization: React.FC = () => {
   // Fetch initial grid data from API
-
-  // useEffect(() => {
-  //   const fetchGridData = async () => {
-  //     const allDataResponse = await fetch(
-  //       "http://localhost:5001/api/iterations"
-  //     );
-  //     console.log("Grid data:", allDataResponse.json());
-  //   };
-  //   fetchGridData();
-  // }, []);
-
   useEffect(() => {
     const fetchGridData = async () => {
       const allDataResponse = await fetch(
@@ -31,13 +20,19 @@ const ResultsVisualization: React.FC = () => {
 
   return (
     <div className="container">
-      <div className="grid">
-        {/* paste your code here */}
-        Test 1
+      <div className="header">
+        <button className="button">temperature</button>
+        <button className="button">pressure</button>
+        <button className="button">temperature 2</button>
       </div>
+      <div className="grid">boxes</div>
       <div className="controls">
-        {/* paste your code here */}
-        Test 2
+        <div>
+          <button className="button">Play</button>
+          <button className="button">Pause</button>
+          <button className="button">Reset</button>
+        </div>
+        <div className="step">step:1</div>
       </div>
     </div>
   );
